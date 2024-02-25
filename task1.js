374b4b4fd4906933c7f87bb8e063348fe9d5780e
DAsha <enyalya@gmail.com>
2024-02-25 18:13:48 +0200

    added 3 colors

---
+++ task1.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+console.log(
+  chalk.green("Love is not about possession. Love is about appreciation.")
+);
+console.log(chalk.red("Fall seven times and stand up eight"));
+console.log(chalk.blue("The only way to do great work is to love what you do"));
