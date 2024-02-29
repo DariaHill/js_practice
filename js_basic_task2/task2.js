92680ef555893715c38bb7647ee35ba46765eea3
DAsha <enyalya@gmail.com>
2024-02-29 12:12:26 +0200

    added task2

--- js_basic_task2/task2.js
+++ js_basic_task2/task2.js
@@ -1,8 +1,45 @@
 let basicValue = 10000;
-let yearOfManufacture;
+let yearOfManufacture = 2023;
+let mileage = 110000;
+let climateСontrol = true;
+let electricCar = false;
+let securityPackage = true;
 
 if (yearOfManufacture >= 2019 && yearOfManufacture <= 2022) {
   yearOfManufacture = basicValue + (basicValue * 10) / 100;
-  message = "The cost of your car";
-  console.log("message ${yearOfManufacture} грн");
+
+  console.log(basicValue);
+  console.log(
+    `The final price of the car relative to the year of manufacture ${yearOfManufacture} грн`
+  );
+}
+if (mileage >= 50000 && mileage <= 100000) {
+  mileage = basicValue - (basicValue * 5) / 100;
+  console.log(basicValue);
+  console.log(
+    `The final price of the car relative to the mileage is ${mileage} грн`
+  );
+}
+if (climateСontrol == true) {
+  climateСontrol = basicValue + (basicValue * 7) / 100;
+  console.log(basicValue);
+  console.log(
+    `The final price of the car relative to theclimate control is ${climateСontrol}`
+  );
+} else {
+  console.log(basicValue);
+}
+if (securityPackage == true) {
+  securityPackage = basicValue + (basicValue * 8) / 100;
+  console.log(basicValue);
+  console.log(
+    `The final price of the car relative to the security package is ${mileage} грн`
+  );
+}
+if (electricCar == true) {
+  electricCar = basicValue + (basicValue * 15) / 100;
+  console.log(basicValue);
+  console.log(
+    `The final price of the car relative to the electric car is ${mileage} грн`
+  );
 }
