f4f9ef70e870b6244d415e07f606f643f6c289d4
Daria Babenko <enyalya@gmail.com>
2024-03-11 15:08:18 +0200

    task1 modified

--- js_basic_task4/task1.js
+++ js_basic_task4/task1.js
@@ -1,5 +1,22 @@
 /* Використовуючи звичайний цикл for, порахуйте кількість позитивних та негативних чисел та нулів.
  */
 
-const numbers=[5,-3,7,0,-4,9,8,-9];
-for(let i=0; i<=numbers.length; i++);
+let numbers=[5,-3,7,0,-4,9,8,-9];
+
+let positiveCount=0;
+let negativeCount=0;
+let zeroCount=0;
+
+for(let i=0; i<numbers.length; i++){
+    if (numbers[i]>0){
+        positiveCount++;
+    }else if (numbers[i]<0){
+        negativeCount++;
+    }else {
+            zeroCount++
+        } 
+}
+
+console.log("Positive numbers:" +positiveCount);
+console.log("Negative numbers:" +negativeCount);
+console.log("Zeros: " + zeroCount);
