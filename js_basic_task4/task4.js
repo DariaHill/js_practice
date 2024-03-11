9c9313a9da1465eb0aad3acb562c9dd43a5aaee7
Daria Babenko <enyalya@gmail.com>
2024-03-11 16:13:42 +0200

    task 4 modified

--- js_basic_task4/task4.js
+++ js_basic_task4/task4.js
@@ -1,5 +1,5 @@
 /* Використовуючи .find(), виведіть перше ім'я, з довжиною більше 6 символів */
 
 const names = ["John", "Fernando", "Alexandra", "Anatolii", "Arina", "Elon"];
-const nameMore= names.find(name => name.length < 6);
+const nameMore= names.find(name => name.length > 6);
 console.log(nameMore);
